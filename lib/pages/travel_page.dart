507e12959b87f57d05ff8ba6adc5a6ccca49d69a
boomhe <ahedong@126.com>
2024-04-15 21:43:23 +0800

    feat : 旅拍接口

--- lib/pages/travel_page.dart
+++ lib/pages/travel_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import '../dao/travel_dao.dart';
+import '../model/travel_category_model.dart';
+
 /// 旅拍页面
 class TravelPage extends StatefulWidget {
   const TravelPage({super.key});
@@ -9,6 +12,22 @@ class TravelPage extends StatefulWidget {
 }
 
 class _TravelPageState extends State<TravelPage> {
+  List<TravelTab> tabs = [];
+  TravelCategoryModel? travelTabModel;
+
+  @override
+  void initState() {
+    super.initState();
+    TravelDao.getCategory().then((TravelCategoryModel? model) {
+      setState(() {
+        travelTabModel = model;
+        tabs = model?.tabs ?? [];
+      });
+    }).catchError((e) {
+      debugPrint(e.toString());
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
